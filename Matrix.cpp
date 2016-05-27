fa5f0d18880dbd55c8deeecda71ae4bfa65d1d1b
nikolai96291 <nikolai96292@gmail.com>
2016-05-27 15:21:44 +0600

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 
-class Matrix 
+class Matrix
 {
 	int n;
 	int m;
@@ -36,26 +37,26 @@ public:
 		}
 	}
 	virtual Matrix& operator = (Matrix& a)
+	{
+		float tmp;
+		n = a.n;
+		m = a.m;
+		data = new float[n*m];
+		for (int j = 0; j < m; j++)
 		{
-			float tmp;
-			n = a.n;
-			m = a.m;
-			data = new float[n*m];
-			for (int j = 0; j < m; j++)
+			for (int i = 0; i < n; i++)
 			{
-				for (int i = 0; i < n; i++)
-				{
-					tmp = a.data[j*n + i];
-					set(i, j, tmp);
-				}
+				tmp = a.data[j*n + i];
+				set(i, j, tmp);
 			}
-			return (*this);
 		}
+		return (*this);
+	}
 	virtual Matrix operator + (Matrix& a)
 	{
 		if (n == a.getN() && m == a.getM())
 		{
-			Matrix c(n,m);
+			Matrix c(n, m);
 			float tmp;
 			for (int j = 0; j < m; j++)
 			{
@@ -95,12 +96,28 @@ public:
 			return c;
 		}
 	}
+	virtual Matrix operator* (float c)
+	{
+		if (failed() == 1)
+		{
+			return *this;
+		}
+		Matrix res;
+		res.n = this->n;
+		res.m = this->m;
+		res.data = new float[res.n*res.m];
+		for (int i(0); i < res.n*res.m; i++)
+		{
+			res.data[i] = this->data[i] * c;
+		}
+		return Matrix(res);
+	}
 	virtual Matrix operator * (Matrix& a)
 	{
-		
+
 		if (n == a.getM())
 		{
-			Matrix c(m,a.getN());
+			Matrix c(m, a.getN());
 			float k = 0;
 			for (int j = 0; j < m; j++)
 			{
@@ -119,7 +136,7 @@ public:
 			Matrix c;
 			return c;
 		}
-	
+
 	}
 	virtual Matrix transpose()
 	{
@@ -162,6 +179,59 @@ public:
 		}
 		return(o);
 	}
+	virtual Matrix GetMinor(int _i, int _j)
+	{
+		if (n != m)
+		{
+			return *(this);
+		}
+		Matrix temp(n - 1, m - 1);
+		int k = 0;
+		int p = 0;
+		for (int i(0); i < n; i++)
+		{
+			if (i == _i)
+				continue;
+			p = 0;
+			for (int j(0); j < m; j++)
+			{
+				if (j == _j)
+					continue;
+				temp.set(k, p, get(i, j));
+				p++;
+			}
+			k++;
+		}
+		return Matrix(temp);
+	}
+	virtual double determinant()
+	{
+		if (n != m || failed() == 1)
+			return 0;
+		if (n == 1)
+			return data[0];
+		double det = 0;
+		for (int i(0); i < n; i++)
+			det = det + powf(-1, i)*data[i] * GetMinor(0, i).determinant();
+		return det;
+	}
+	virtual Matrix reverse()
+	{
+		if (failed() == 1)
+			return *this;
+		double det = determinant();
+		if (det == 0)
+			return *this;
+		Matrix _inverse(n, m);
+		for (int i(0); i < n; i++)
+			for (int j(0); j < m; j++)
+			{
+			_inverse.set(i, j, powf(-1, i + j) * GetMinor(j, i).determinant());
+			}
+
+		_inverse = _inverse*pow(det, -1);
+		return Matrix(_inverse);
+	}
 	virtual void set(int i, int j, float data)
 	{
 		this->data[j*this->n + i] = data;
@@ -180,9 +250,7 @@ public:
 	}
 	virtual bool failed()
 	{
-		if ((n == 0 && m == 0) || data == NULL)
-			return true;
-		else
-			return false;
+		return (n <= 0 || m <= 0 || n != m || data == NULL);
 	}
+
 };
